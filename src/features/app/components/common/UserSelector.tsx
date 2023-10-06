db8ab70ce7c842a6c139b1ace4743c4f059ef81e
Diego Díaz <diego@diegodiazweb.com>
2023-10-06 12:36:48 -0300

    - integrate tw variants to create components - implement common wrapper to user selector items

--- src/features/app/components/common/UserSelector.tsx
+++ src/features/app/components/common/UserSelector.tsx
@@ -1,10 +1,15 @@
 import {FC, useMemo} from 'react';
 import {Avatar, Dropdown} from 'flowbite-react';
+import {tv} from 'tailwind-variants';
 
 import Loading from 'features/app/components/common/Loading';
 import {USER_NOT_ASSIGNED} from 'features/app/constants/users-constants';
 import useUsersData from 'features/app/hooks/useUsersData';
 import {UserProps} from 'lib/sdk/users/client/get';
+
+const userSelectorItem = tv({
+  base: ['grid', 'grid-auto-col ', 'gap-1', ' grid-cols-[2rem_1fr] ', 'cursor-pointer']
+});
 
 type UserSelectorProps = {
   userId: UserProps['id'] | null;
@@ -30,7 +35,7 @@ const UserSelector: FC<UserSelectorProps> = ({userId, showUserName = false, onCh
       label="Dropdown"
       inline
       renderTrigger={() => (
-        <div className="grid grid-auto-col gap-1 grid-cols-[2rem_1fr] cursor-pointer">
+        <div className={userSelectorItem()}>
           <Avatar rounded size="xs" img={currentUser.avatar_url} alt={currentUser.user_name} />
           {showUserName && <p className="m-0">{currentUser.user_name}</p>}
         </div>
@@ -41,13 +46,22 @@ const UserSelector: FC<UserSelectorProps> = ({userId, showUserName = false, onCh
         value={USER_NOT_ASSIGNED.id}
         onClick={() => handleUserChange(USER_NOT_ASSIGNED.id)}
       >
-        <Avatar rounded size="xs" img={USER_NOT_ASSIGNED.avatar_url} alt={USER_NOT_ASSIGNED.name} />
-        {USER_NOT_ASSIGNED.name}
+        <div className={userSelectorItem()}>
+          <Avatar
+            rounded
+            size="xs"
+            img={USER_NOT_ASSIGNED.avatar_url}
+            alt={USER_NOT_ASSIGNED.name}
+          />
+          {USER_NOT_ASSIGNED.name}
+        </div>
       </Dropdown.Item>
       {users?.map(({id, user_name, avatar_url}) => (
         <Dropdown.Item key={id} value={id} onClick={() => handleUserChange(id)}>
-          <Avatar rounded size="xs" img={avatar_url} alt={user_name} />
-          {user_name}
+          <div className={userSelectorItem()}>
+            <Avatar rounded size="xs" img={avatar_url} alt={user_name} />
+            {user_name}
+          </div>
         </Dropdown.Item>
       ))}
     </Dropdown>
